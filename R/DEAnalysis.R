679acedede6956dcb66c91c977d6b74646775cd6
lisiarend <lisiarend@me.com>
2023-10-11 14:04:33 +0200

    comparison order changed

--- R/DEAnalysis.R
+++ R/DEAnalysis.R
@@ -48,7 +48,7 @@ specify_comparisons <- function(se, condition = NULL, sep = NULL, control = NULL
           }
         }
         if(match_count == must_match){
-          comparisons <- c(comparisons, paste0(sample_a, "-", sample_b))
+          comparisons <- c(comparisons, paste0(sample_b, "-", sample_a))
         }
       }
     }
